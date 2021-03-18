8f895f8c1337f7a58be163944a4c6b220bcd9334
juan010899 <80277027+juan010899@users.noreply.github.com>
2021-03-18 12:14:36 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -27,8 +27,8 @@ function LED1_Off(){
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "lfrenteriax@hotmail.com",
-    password: "lfrenteriax",
+    userName: "orozcojuanpablo817@gmail.com",
+    password: "Aplicado55",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -41,9 +41,9 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("lfrenteriax@hotmail.com/test");
+    client.subscribe("orozcojuanpablo817@gmail.com/tema1");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "orozcojuanpablo817@gmail.com/tema1";
     client.send(message);
 	
   }
