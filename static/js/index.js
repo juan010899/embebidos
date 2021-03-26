f02d3bc87ff552595ab8946979936de1c13e560a
juan010899 <80277027+juan010899@users.noreply.github.com>
2021-03-26 15:04:19 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -107,23 +107,25 @@ function LED1_Off(){
   // called when a message arrives
  function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString); 
+	 document.getElementById("sensor").innerHTML=message.payloadString;
+    console.log("onMessageArrived:"+message.payloadString1); 
+	 document.getElementById("sensor2").innerHTML=message.payloadString1;	 
 	  //document.getElementById("sensor").innerHTML=message.payloadString;
 	  //if(message.payloadString===' EncendidoLed1'){
 		//  document.getElementById("imagen").src="http://www.clker.com/cliparts/D/M/r/s/n/P/led-red-off-md.png";
 	  //} else if(message.payloadString===' ApagadoLed1'){
 		//  document.getElementById("imagen").src="http://www.clker.com/cliparts/D/M/r/s/n/P/led-red-off-md.png";
 	  //}
-    document.getElementById("sensor").innerHTML=message.payloadString;		 
+    	 
 	  if(message.payloadString===' EncendidoLed1'){
 		  document.getElementById("btn").innerHTML="Apagar";
 	  }else if(message.payloadString===' ApagadoLed1'){
 		  document.getElementById("btn").innerHTML="Encender";
 	  }
-    document.getElementById("sensor2").innerHTML=message.payloadString;
           if(message.payloadString===' EncendidoLed2'){
-		  document.getElementById("btn2").innerHTML="Apagar";
+		   document.getElementById("btn2").innerHTML="Apagar";
 	  }else if(message.payloadString===' ApagadoLed2'){
-		  document.getElementById("btn2").innerHTML="Encender";
+		   document.getElementById("btn2").innerHTML="Encender";
 	  }
 	 
   }
