1204f4cca1dd5a8381c977937279399142d98927
juan010899 <80277027+juan010899@users.noreply.github.com>
2021-03-26 17:45:35 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -121,13 +121,14 @@ function LED1_Off(){
 	  }else if(message.payloadString===' ApagadoLed1'){
 		  document.getElementById("sensor").innerHTML="apagadoled2";
 		  document.getElementById("btn").innerHTML="Apagado";
-          }if(message.payloadString===' EncendidoLed2'){       
+          }
+	 if(message.payloadString===' EncendidoLed2'){       
 		  document.getElementById("sensor2").innerHTML="encendidoled2";
 		  document.getElementById("btn2").innerHTML="Encendido";
-          }else if(message.payloadString===' ApagadoLed2'){       
+        }else if(message.payloadString===' ApagadoLed2'){       
 		  document.getElementById("sensor2").innerHTML="apagadoled2";
 		  document.getElementById("btn2").innerHTML="Apagado";
-	  }
+	 }
 	 
   }
 
